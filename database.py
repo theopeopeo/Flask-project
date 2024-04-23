73e1926bfe5a7790edf54250c830499f1284b357
Your Name <your.email@example.com>
2024-04-23 12:45:34 +0200

    added posting applications

--- database.py
+++ database.py
@@ -22,7 +22,19 @@ def load_job_from_db(id):
             return None
         else:
             return dict(rows[0]._asdict())
+
+def add_application_to_db(job_id, data):
+     with engine.connect() as conn:
+        query = text("insert into applications (job_id, full_name, email, linkedin_url, education, work_experience, resume_url) values(:job_id, :name, :email, :linkedin_url, :education, :work_experience, :resume_url)")
         
+        conn.execute(query, job_id=job_id,
+                     full_name=data['full_name'],
+                     email=data['email'],
+                     linkedin_url=data['linkedin_url'],
+                     education=data['education'],
+                     work_experience=data['work_experience'],
+                     resume_url=data['resume_url'])
+    
         
        
         
