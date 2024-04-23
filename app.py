4a26dbe2e4c6de2ce609b6e5434aa2877a76f366
Your Name <your.email@example.com>
2024-04-23 09:00:49 +0200

    succesfully connected supabase and displayed on webpage

--- app.py
+++ app.py
@@ -1,58 +1,20 @@
-from flask import Flask, render_template, jsonify, request
-from supabase import create_client, Client
+from flask import Flask, render_template, jsonify
+from database import load_jobs_from_db
+
 
 
 
 app = Flask(__name__)
-url: str = "https://shqvhviglspulcejfrmv.supabase.co"
-key: str = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNocXZodmlnbHNwdWxjZWpmcm12Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM2ODQ2NTEsImV4cCI6MjAyOTI2MDY1MX0.pv-aL_tLxpPVwvpIU6iJoW-_fPCiDLuiiS3U0LgjfhY"
-supabase: Client = create_client(url, key)
 
-JOBS = [
-    {
-        "id": 1,
-        "title": "Data Analyst",
-        "location": "Bengaluru, India",
-        "salary": "Rs. 10,00,000",
-    },
-    {
-        "id": 2,
-        "title": "Data Scientist",
-        "location": "Delhi, India",
-        "salary": "Rs. 15,00,000",
-    },
-    {
-        "id": 3,
-        "title": "Frontend Engineer",
-        "location": "Remote",
-        "salary": "Rs. 12,00,000",
-    },
-    {
-        "id": 4,
-        "title": "Backend Engineer",
-        "location": "San Francisco, USA",
-        "salary": "$120,000",
-    },
-]
     
 @app.route("/")
 def hello_world():
-    return render_template("home.html", jobs=JOBS, company_name="Jovian")
+    job_list = load_jobs_from_db()
+    return render_template("home.html", jobs=job_list, company_name="Jovian")
 
-@app.route("/api/jobs")
+"""@app.route("/api/jobs")
 def list_jobs():
-    return jsonify(JOBS)
-
-@app.route('/users', methods=['POST'])
-def add_user():
-    username = request.json['username']  # Make sure client sends JSON with a 'username' key
-    data = supabase.table("users").insert({"username": username}).execute()
-    return jsonify(data.data), 201
-
-@app.route('/users', methods=['GET'])
-def get_users():
-    data = supabase.table("users").select("*").execute()
-    return jsonify(data.data)
+    return jsonify(jobs)"""
 
 
 if __name__ == "__main__":
--- database.py
+++ database.py
@@ -1,7 +1,16 @@
 from sqlalchemy import create_engine, text
-engine = create_engine("postgresql+psycopg2://postgres.shqvhviglspulcejfrmv:Teddydaniels1886!!@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres")
 
-with engine.connect() as conn:
-    print(conn)
-    result = conn.execute(text("select * from job_postings"))
-    print(result.all())
+
+engine = create_engine("postgresql+psycopg2://postgres.shqvhviglspulcejfrmv:Teddydaniels1886!@aws-0-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require", echo=True, connect_args={"connect_timeout": 10})
+
+
+def load_jobs_from_db():
+    with engine.connect() as conn:
+        result = conn.execute(text("select * from job_postings"))
+        jobs = []
+        for row in result.all():
+            jobs.append(dict(row._asdict()))
+        print(jobs)
+        return jobs
+       
+        
